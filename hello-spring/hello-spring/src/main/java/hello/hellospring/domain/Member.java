9b9bffe46973ef665535be4290184a368f1d5c77
Dahye Kim <digeuthi@gmail.com>
2023-07-28 20:00:30 +0900

    jpa study start

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity //jpa를 사용할때 이 어노테이션을 추가하면 이제 jpa가 관리하는 entity가 되는것.
 public class Member {
 
+    //jpa : java 인터페이스의 표준, 구현을 여러업체에서 한다고 생각하면 된다.
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //고객이정하는 아이디 아니고 시스템이 저장하는 아이디
 
+    //@Column(name = "user") DB에 User라는 이름으로 컬럼명 저장시 이렇게 맵핑한다.
     private String name;
 
     public Long getId() {
---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    private final EntityManager em; //jpa는 EntityManager로 모든 것이 동작한다.
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
