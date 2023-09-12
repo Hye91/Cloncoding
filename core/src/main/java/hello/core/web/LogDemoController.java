890f6a584306ca2e0022c852716b309fb44b817c
Dahye Kim <digeuthi@gmail.com>
2023-09-12 18:18:23 +0900

    proxy scope

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -15,15 +15,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    //private final MyLogger myLogger; request가 들어왔을때만 실행이 되는 상황 -> provider로 DL을 하면 된다.
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger; //request가 들어왔을때만 실행이 되는 상황 -> provider로 DL을 하면 된다.
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody //화면 없이 텍스트로만 반환받으려고 함.
     public String logDemo(HttpServletRequest request) throws InterruptedException {
-        MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 myLogger을 받을수있다,
+        //MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 myLogger을 받을수있다,
         //HttpServletRequest 자바 규약에 의한 http request 요청 정보를 받을 수 있다.
         String requestURL = request.getRequestURL().toString();
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
