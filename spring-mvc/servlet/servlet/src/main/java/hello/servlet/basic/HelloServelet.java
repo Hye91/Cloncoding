767380c765de0c6ca596f8007331d13d331551dd
Dahye Kim <digeuthi@gmail.com>
2023-11-02 11:58:31 +0900

    servlet

--- spring-mvc/servlet/servlet/src/main/java/hello/servlet/basic/HelloServelet.java
+++ spring-mvc/servlet/servlet/src/main/java/hello/servlet/basic/HelloServelet.java
@@ -16,5 +16,15 @@ public class HelloServelet extends HttpServlet {
         System.out.println("HelloServelet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
+
+        String username = request.getParameter("username"); //ctrl + arl + v : 위에 단계 꺼내기였나
+        //request를 queryParameter로 요청을 보내는걸 가능하게 한다
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //1. 응답을 단순 텍스트 타입으로 받게된다
+        response.setCharacterEncoding("utf-8"); //2. 인코딩 정보를 알려주게 된다 -> 1과 2는 헤더 정보에 들어가게 된다
+        
+        response.getWriter().write("hello " + username);//http 메시지 바디에 텍스트가 들어가게된다
     }
+
 }
