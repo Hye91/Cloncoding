c1a2e8eb43499d01ab45c114b96b1f38e4fd6448
Dahye Kim <digeuthi@gmail.com>
2023-12-05 13:33:04 +0900

    thymeleaf study

--- spring-mvc/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,4 +1,44 @@
 package hello.itemservice.web.basic;
 
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
 public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    //@Autowired //스프링에 등록된 컴포넌트끼리 관계주입(생성자 관계주입), 생성자가 1개인 경우 생략가능
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    } @Requirde~ 를 쓰면 final이 붙은 인스턴스의 생성자를 만들어준다
+
+    @GetMapping //아이템 목록 출력
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /***
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA",10000,10));
+        itemRepository.save(new Item("itemB",20000,20));
+        itemRepository.save(new Item("itemC",30000,30));
+    }
 }
