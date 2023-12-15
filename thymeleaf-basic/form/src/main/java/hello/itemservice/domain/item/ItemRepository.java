183d337cb3aa3644855cad2fc56f2d1aad653dea
Dahye Kim <digeuthi@gmail.com>
2023-12-15 17:45:46 +0900

    thyemeleaf single checkBox

--- thymeleaf-basic/form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ thymeleaf-basic/form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -28,10 +28,16 @@ public class ItemRepository {
     }
 
     public void update(Long itemId, Item updateParam) {
+        //updateParam : 수정이 되어야할 요소들을 한번에 객체로 가져오는 그런거 만들어둔건가?
+        //updateParam 객체가 수정할 모든 필드를 캡슐화하며 호출자에게 새 값이 들어 있는 단일 객체를 제공
         Item findItem = findById(itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setOpen(updateParam.getOpen()); //상품 수정에서 판매여부를 수정한다음 적용되게 한다.!
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
