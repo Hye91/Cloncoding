2ab08d63f17dbb3e2121acedf99a71a3bf90c40a
Dahye Kim <digeuthi@gmail.com>
2023-12-31 11:36:21 +0900

    bean Validation groups

--- spring-mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring-mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -14,18 +14,21 @@ import javax.validation.constraints.NotNull;
 //기능이 너무 약하기때문에 주석처리함 , _this.는 지금 내 객체를 의미함
 public class Item {
 
+    //beanValidation의 groups 기능을 사용해서 각 기능별로 그룹을 나누어서 적용시킬수 있다.
+
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message = "공백X")
+    @NotBlank(message = "공백X",groups = {SaveCheck.class, UpdateCheck.class})
     // 문자열 값이 null이 아니며, 최소한 하나의 공백이 아닌 문자를 포함하고 있는지를 확인합니다.
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000,groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999 , groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
--- spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -41,10 +43,39 @@ public class ValidationItemControllerV3 {
         return "validation/v3/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItem(@Validated/*Item에 대해서 검증기가 적용된다*/ @ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes, Model model) {
         //@Validated를 사용하면 beanValidation이 다른 검증기를 추가하지 않아도 바로 적용이 되게 된다.
+
+        //특정 필드가 아닌 복합 룰 검증 (globalError)
+        //objectError은 값이 남아있는게 없기때문에 rejectedValue나 bindingFailure 추가하지않는다
+        if(item.getQuantity() != null && item.getPrice() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin",new Object[]{10000,resultPrice},null);
+                //reject는 Object , Object 이름은 bindingResult가 이미 알고 있기때문에 여기서 끝
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로! 오류값또한 같이 입력폼에 보이도록 보내줘야한다.
+        if(bindingResult.hasErrors()/*!errors.isEmpty()*/){
+            log.info("errors ={} " , bindingResult);
+            return "validation/v3/addForm"; //검증 실패시 그냥 입력뷰로 보내버리는것
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItem2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes, Model model) {
+        //@Validated를 사용하면 beanValidation이 다른 검증기를 추가하지 않아도 바로 적용이 되게 된다.
+        //@Validated(SaveCheck.class)이렇게 되면 SaveCheck.class인 조건만 사용되게 된다
 
         //특정 필드가 아닌 복합 룰 검증 (globalError)
         //objectError은 값이 남아있는게 없기때문에 rejectedValue나 bindingFailure 추가하지않는다
@@ -76,8 +107,31 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+    //@PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //특정 필드가 아닌 복합 룰 검증 (globalError)
+        //objectError은 값이 남아있는게 없기때문에 rejectedValue나 bindingFailure 추가하지않는다
+        if(item.getQuantity() != null && item.getPrice() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin",new Object[]{10000,resultPrice},null);
+                //reject는 Object , Object 이름은 bindingResult가 이미 알고 있기때문에 여기서 끝
+            }
+        }
+
+        //오류가 있는 경우 폼으로 돌아가기
+        if(bindingResult.hasErrors()){
+            log.info( "errors = {}" + bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
 
         //특정 필드가 아닌 복합 룰 검증 (globalError)
         //objectError은 값이 남아있는게 없기때문에 rejectedValue나 bindingFailure 추가하지않는다
