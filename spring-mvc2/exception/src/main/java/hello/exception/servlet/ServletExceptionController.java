0122d03271eac1021a27246f50047f1fe3a6875c
Dahye Kim <digeuthi@gmail.com>
2024-01-07 10:02:03 +0900

    servlet Exception

---
+++ spring-mvc2/exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -0,0 +1,30 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExceptionController {
+
+    @GetMapping("/error-ex")
+    public void errorEx(){
+        throw new RuntimeException("예외 발생!"); //예외를 그냥 던져주는것 이때는 500상태의 에러가 뜬다
+
+    }
+
+    @GetMapping("/error-404") //HttpServletResponse 가 제공하는 sendError 라는 메서드를 사용
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404,"404 오류!");
+        //sendError을 통해서 내가 원하는 오류 상태코드를 직접 던져주는것 가능
+    }
+
+    @GetMapping("/error-500") //HttpServletResponse 가 제공하는 sendError 라는 메서드를 사용
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500,"500 오류!");
+    }
+}
