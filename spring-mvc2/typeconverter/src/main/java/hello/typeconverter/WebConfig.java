ae62aee57dc05d045057e1208f59cb942fce0fc4
Dahye Kim <digeuthi@gmail.com>
2024-01-12 14:15:44 +0900

    formatter apply

--- spring-mvc2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ spring-mvc2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) { //converter의 조금 더 확장된 기능
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter()); 우선순위 처리로 인해서 주석처리
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
+        //기존에 만들었던 converter 중에서 숫자 문자 관련 converter이 있는데 그게 우선순위가 되어버리면
+        //안되기때문에 formatter을 등록하면서 주석처리 해주었다.(converter가 우선순위)
     }
 }
