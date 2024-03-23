deffd4c63f1235b211f62979a6278be8bfeabf4f
Dahye Kim <digeuthi@gmail.com>
2024-03-23 18:13:01 +0900

    DataBase Integration

--- spring-db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ spring-db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -13,9 +13,14 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest //이 어노테이션이붙어있으면 @SpringBootApplication 어노테이션을 찾아내서 설정으로 사용하게 된다.
+    //그래서 @SpringBootApplication에 설정되어있는 import설정으로 테스트하게된다
 class ItemRepositoryTest {
 
+    /**
+     * 테스트에서 중요한건 격리성! 테스트를 실행할때 기존에 저장되어있는 값이 방해가 되면 안된다
+     * 데이터가 깔끔해야 테스트를 방해하지 않고 원활한 진행이 되게 된다.
+     */
     @Autowired
     ItemRepository itemRepository;
 
@@ -71,6 +76,8 @@ class ItemRepositoryTest {
 
         //둘 다 없음 검증
         test(null, null, item1, item2, item3);
+        //이름과 가격값이 null인 경우에는 item1~3까지 3개의 데이터가 있어야하는데 JdbcTemplate를 사용해서
+        //테스트를 하게되면 이 세값 이외에도 많은 값들이 기존에 저장되어 있기때문에 오류가 생기게된다.
         test("", null, item1, item2, item3);
 
         //itemName 검증
@@ -86,6 +93,7 @@ class ItemRepositoryTest {
     }
 
     void test(String itemName, Integer maxPrice, Item... items) {
+        //검색의 조건을 입력하고, 이후에는 입련되어있는 아이템 항목을 나열하게된다.
         List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
         assertThat(result).containsExactly(items); //Exactly를 하면 순서도 다 맞아야한다.
     }
