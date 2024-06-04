89ceae34cb8bd20f873c427c164cefc944cfa6c2
Dahye Kim <digeuthi@gmail.com>
2024-06-04 17:43:46 +0900

    state study

--- toonflix/lib/widgets/state_ful.dart
+++ toonflix/lib/widgets/state_ful.dart
@@ -13,12 +13,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  // int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
       //위젯이 변경되었음을 알리는 함수
-      counter = counter + 1;
+      // counter = counter + 1;
+      numbers.add(numbers.length);
     }); //함수를 setState에 넣어야하는건 아니지만, 넣어야 가독성이 좋아진다
   }
 
@@ -35,10 +37,11 @@ class _AppState extends State<App> {
                 'Clik Count',
                 style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+              // Text(
+              //   '$counter',
+              //   style: const TextStyle(fontSize: 30),
+              // ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
