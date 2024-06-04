caf45adc75922f038467db5689894f5d92fb726c
Dahye Kim <digeuthi@gmail.com>
2024-06-04 17:10:22 +0900

    setState button

--- toonflix/lib/widgets/state_ful.dart
+++ toonflix/lib/widgets/state_ful.dart
@@ -16,7 +16,10 @@ class _AppState extends State<App> {
   int counter = 0;
 
   void onClicked() {
-    counter = counter + 1;
+    setState(() {
+      //위젯이 변경되었음을 알리는 함수
+      counter = counter + 1;
+    }); //함수를 setState에 넣어야하는건 아니지만, 넣어야 가독성이 좋아진다
   }
 
   @override
