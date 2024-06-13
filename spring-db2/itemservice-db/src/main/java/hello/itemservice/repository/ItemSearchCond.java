f9dc914191775f87872d7bddb5c0c89a7f571b78
Dahye Kim <digeuthi@gmail.com>
2024-06-13 15:14:34 +0900

    check repository

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/ItemSearchCond.java
@@ -14,5 +14,6 @@ public class ItemSearchCond {
     public ItemSearchCond(String itemName, Integer maxPrice) {
         this.itemName = itemName;
         this.maxPrice = maxPrice;
+        //test
     }
 }
